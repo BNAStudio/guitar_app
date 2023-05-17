d92d33d538c3d0e651fabc3c9d6d1ac1714cee4b
bb_coor_frontend <coor_frontend@booksandbooksdigital.com.co>
2023-05-17 13:25:38 -0500

    feat(components|pages): adding new components and styles

--- components/footer.js
+++ components/footer.js
@@ -1,5 +1,27 @@
+import Link from "next/link"
+import styles from "../styles/footer.module.css"
+
+const year = new Date().getFullYear()
 export default function Footer() {
     return (
-        <div>footer</div>
+        <footer className={styles.footer}>
+            <div className={`contenedor ${styles.contenido}`}>
+                <nav className={styles.navegacion}>
+                    <Link legacyBehavior href="/">
+                        Home
+                    </Link>
+                    <Link legacyBehavior href="/nosotros">
+                        Nosotros
+                    </Link>
+                    <Link legacyBehavior href="/blog">
+                        Blog
+                    </Link>
+                    <Link legacyBehavior href="/tienda">
+                        Tienda
+                    </Link>
+                </nav>
+                <p className={styles.copyright}>Todos los derechos reservados {year}</p>
+            </div>
+        </footer>
     )
 }
